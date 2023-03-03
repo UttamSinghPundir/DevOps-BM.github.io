98693f1bb67d40c499b57ea42f9186a203669495
Uttam Singh Pundir <96731122+UttamSinghPundir@users.noreply.github.com>
2023-03-03 11:59:56 +0530

    Update responses.js

--- static/scripts/responses.js
+++ static/scripts/responses.js
@@ -14,7 +14,17 @@ function getBotResponse(input) {
         return "Talk to you later!";
     }
     else if (input == "stressed") {
-        return "Hey there calm down i am here to help you out";
+        return "I'm sorry to hear that you're feeling stressed. Stress is a common experience for many people, and it can be caused by a variety of factors such as work, relationships, or health concerns. It's important to take care of yourself when you're feeling stressed, both physically and mentally. Here are some tips that may help:
+
+Take a break: Sometimes the best thing you can do when you're feeling stressed is to take a break. Step away from whatever is causing your stress and do something that relaxes you, such as going for a walk, listening to music, or practicing deep breathing exercises.
+
+Get some exercise: Exercise can be a great way to reduce stress. Even a short walk or some light stretching can help release tension and improve your mood.
+
+Talk to someone: Talking to a trusted friend or family member about what's causing your stress can be helpful. They may be able to offer support, advice, or simply a listening ear.
+
+Practice mindfulness: Mindfulness techniques such as meditation or yoga can help you stay grounded and focused, even in the midst of stressful situations.
+
+Remember that it's okay to feel stressed sometimes, but it's important to take steps to manage your stress in a healthy way. If you're feeling overwhelmed or if your stress is affecting your daily life, consider talking to a mental health professional who can help you develop a personalized plan for managing your stress.";
     }
     else if (input == "focus-problem") {
         return "Focus is a big problem now a days, do not panick i got your back. Just relax and listen our playlist";
